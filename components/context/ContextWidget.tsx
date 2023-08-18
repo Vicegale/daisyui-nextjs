e432bf238b39ee3cff75da0f421eced41311a55f
Vicegale <joserodrigues1997@gmail.com>
2023-08-18 01:50:16 +0100

    1 file modified

--- components/context/ContextWidget.tsx
+++ components/context/ContextWidget.tsx
@@ -1,9 +1,25 @@
-import { useState, createContext } from 'react';
+import { useState, createContext, useContext  } from 'react';
 
+const ColorContext = createContext(null);
 const ContextWidget = () => {
-  const [color, setColor] = useState('red');
-  const ColorContext = createContext(null);
-  return <></>;
+  const [word, setWord] = useState('wild');
+  return (
+    <>
+      <div>
+        <ColorContext.Provider value={word}>
+          <Child/>
+        </ColorContext>
+      </div>
+    </>
+  );
 };
 
+const Child = () => {
+  const ctx = useContext(ColorContext);
+  return (
+    <div>
+      <p>{ctx}</p>
+      </div>
+  )
+}
 export default ContextWidget;
