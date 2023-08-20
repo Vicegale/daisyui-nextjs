cea6e28faff12df7bf45243a127c62000615a49a
Vicegale <joserodrigues1997@gmail.com>
2023-08-20 18:29:29 +0100

    2 files modified

--- components/tasklist/Item.tsx
+++ components/tasklist/Item.tsx
@@ -1,14 +1,21 @@
 import type { Task } from './TaskList';
 import { useEffect, useState } from 'react';
 const Item = (props: { task: Task }) => {
-    const max = 100;
-    const className = (props.task.progress < max) ? "progress-primary" : "progress-secondary";
-    return (
-        <div>
-            <span>{props.task.id}</span>
-            <progress className={"progress w-56 " + className} value={props.task.progress} max="100"></progress>
-            <span>{props.task.progress < 100 ? props.task.progress + "%" : "Complete!"}</span>
-        </div>
-    )
+  const max = 100;
+  const className =
+    props.task.progress < max ? 'progress-primary' : 'progress-secondary';
+  return (
+    <div className="flex flex-row items-center space-x-3">
+      <p className="w-8 text-center">{props.task.id}</p>
+      <progress
+        className={'progress w-56 ' + className}
+        value={props.task.progress}
+        max="100"
+      ></progress>
+      <p className="text-center">
+        {props.task.progress < 100 ? props.task.progress + '%' : 'Complete!'}
+      </p>
+    </div>
+  );
 };
 export default Item;
--- components/tasklist/TaskList.tsx
+++ components/tasklist/TaskList.tsx
@@ -2,45 +2,60 @@ import { useEffect, useState } from 'react';
 import Item from './Item';
 
 export type Task = {
-    id: number;
-    progress: number;
-}
+  id: number;
+  progress: number;
+};
 
 const TaskList = () => {
-    const [tasks, setTasks] = useState<Task[]>([]);
-    
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setTasks(tasks.map((t: Task) => { 
-                return {id: t.id, progress: (t.progress < 100) ? t.progress + 1 : t.progress}
-            }));        
-        }, 50);
-        return () => clearInterval(intervalId);
-    })
+  const [currentId, setCurrentID] = useState<number>(1);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-    const addTask = () => {
-        setTasks([...tasks, {id: tasks.length + 1, progress: 0}]);
-    }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTasks(
+        tasks.map((t: Task) => {
+          return {
+            id: t.id,
+            progress: t.progress < 100 ? t.progress + 1 : t.progress,
+          };
+        })
+      );
+    }, 50);
+    return () => clearInterval(intervalId);
+  });
 
-    const removeCompleted = () => {
-        setTasks(tasks.filter((t) => {
-            return t.progress !== 100;
-        }));
-    }
+  const addTask = () => {
+    setTasks([...tasks, { id: currentId, progress: 0 }]);
+    setCurrentID(currentId + 1);
+  };
 
-    return (
-        <div className="w-96 m-4 p-2 border-solid border-2 border-primary rounded-lg">
-            <button className="btn btn-primary" onClick={addTask}>Add Task</button>
-            <button className="btn btn-primary" onClick={removeCompleted}>Remove Done</button>
-            <ul>
-                {tasks.map((task: Task) => (
-                    <li key={task.id}>
-                        <Item task={task} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  const removeCompleted = () => {
+    setTasks(
+      tasks.filter((t) => {
+        return t.progress !== 100;
+      })
+    );
+  };
+
+  return (
+    <div className="w-96 m-4 p-2 border-solid border-2 border-primary rounded-lg">
+      <div className="flex flex-row space-x-1">
+        <button className="btn btn-primary w-1/2" onClick={addTask}>
+          Add Task
+        </button>
+        <button className="btn btn-primary w-1/2" onClick={removeCompleted}>
+          Remove Done
+        </button>
+      </div>
+      <ul className="mt-2">
+        {tasks.map((task: Task) => (
+          <li key={task.id}>
+            <Item task={task} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default TaskList;
