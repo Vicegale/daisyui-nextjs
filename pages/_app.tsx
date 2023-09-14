7d58eaa389b1a7cd559e7fc3f22950f2874a2cd6
Vicegale <joserodrigues1997@gmail.com>
2023-09-14 02:13:55 +0100

    meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
